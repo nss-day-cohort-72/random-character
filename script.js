de08d5a2848f13a322816d55e4139e33f0f070bb
Dave Berzack <dave.berzack@nashvillesoftwareschool.com>
2024-08-01 11:33:32 -0400

    build random character functionality

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ const classes = [
     "Thief",
     "Bard"
 ];
+
+const getRandomElement = (array)=> {
+    const index = Math.floor(Math.random()*array.length)
+    return array[index];
+}
+
+const characterRace = getRandomElement(races);
+const characterClass = getRandomElement(classes);
+console.log(`Your character is: ${characterRace} ${characterClass}`);
